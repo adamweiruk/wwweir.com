aa8557874bc48166a7d13549834add078d159426
Adam Weir <adamintain@icloud.com>
2023-03-24 18:17:21 +0000

    Add Meanie to sketch.js

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,8 +1,38 @@
+let random = Math.random
+
+let width = 400, height = 400
+
+class Point{
+  x = random() * width
+  y = random() * height
+  vx = random() * 3
+  vy = random() * 3
+
+  move() {
+    if (this.x >= width || this.x <= 0) {
+      this.vx *= -1
+    }
+    if (this.y >= height || this.y <= 0) {
+      this.vy *= -1
+    }
+    this.x += this.vx
+    this.y += this.vy
+  }
+}
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(width, height)
+    p1 = new Point()
+    p2 = new Point()
+    p3 = new Point()
   }
   
   function draw() {
-    background(220);
-    ellipse(50,50,80,80);
+    background(255)
+    line(p1.x, p1.y, p2.x, p2.y)
+    line(p2.x, p2.y, p3.x, p3.y)
+    line(p3.x, p3.y, p1.x, p1.y)
+    p1.move()
+    p2.move()
+    p3.move()
   }
